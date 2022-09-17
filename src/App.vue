9c5c89987f436f53bd7d00c3f0654fa9ee85179e
abel <abel.hernando@basetis.com>
2022-09-17 12:23:02 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>TASK BOARD</h1>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+};
+</script>
+<style lang="scss"></style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="cn.root"></div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainPage",
+  components: {},
+  setup() {
+    const cn = getClassNames();
+
+    return {
+      cn,
+    };
+  },
+  head: {},
+});
+
+function getClassNames(): Object {
+  const root = "main-page";
+  return {
+    root,
+    listContainer: `${root}__list-container`,
+  };
+}
+</script>
+
+<style lang="scss" scoped></style>
